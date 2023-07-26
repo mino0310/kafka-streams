2563fd0924a81c86da171a47b1246cd311334da4
Minho Kim <bewriter310@gmail.com>
2023-07-26 18:40:37 +0900

    first commit

---
+++ src/main/kotlin/com/example/kakfastreams/KakfaStreamsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kakfastreams
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KakfaStreamsApplication
+
+fun main(args: Array<String>) {
+    runApplication<KakfaStreamsApplication>(*args)
+}
